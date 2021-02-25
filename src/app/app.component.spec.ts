01295b38c7643f3988a35f3d85af85094728dc43
Zijlmans, P. (Paul) <paul.zijlmans@devolksbank.nl>
2021-02-25 16:23:15 +0100

    basic setup

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'recipe-app'`, () => {
+  it(`should have as title 'recipe-book'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('recipe-app');
+    expect(app.title).toEqual('recipe-book');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('recipe-app app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('recipe-book app is running!');
   });
 });
