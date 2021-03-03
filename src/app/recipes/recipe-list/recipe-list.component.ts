6caaaa3f63bde29dcad285434955bc801cc99517
Paul Zijlmans <paulzijlmans@hotmail.com>
2021-03-03 16:07:30 +0100

    services & dependency injection - added recipe service - added shopping list service

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
@@ -9,22 +10,13 @@ import { Recipe } from '../recipe.model';
 export class RecipeListComponent implements OnInit {
   @Output() recipeWasSelected: EventEmitter<Recipe> = new EventEmitter<Recipe>();
 
-  recipes: Recipe[] = [
-    new Recipe(
-      'A Test Recipe',
-      'This is simply a test!',
-      'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg'
-    ),
-    new Recipe(
-      'The second recipe',
-      'This is awesome!',
-      'https://cdn.pixabay.com/photo/2017/07/16/10/43/recipe-2508859_1280.jpg'
-    ),
-  ];
+  recipes: Recipe[];
 
-  constructor() {}
+  constructor(private recipeService: RecipeService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.recipes = this.recipeService.getRecipes();
+  }
 
   onRecipeSelected(recipe: Recipe): void {
     this.recipeWasSelected.emit(recipe);
---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from "@angular/core";
+import { Recipe } from "./recipe.model";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RecipeService {
+  private recipes: Recipe[] = [
+    new Recipe(
+      'A Test Recipe',
+      'This is simply a test!',
+      'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg'
+    ),
+    new Recipe(
+      'The second recipe',
+      'This is awesome!',
+      'https://cdn.pixabay.com/photo/2017/07/16/10/43/recipe-2508859_1280.jpg'
+    ),
+  ];
+
+  getRecipes(): Recipe[] {
+    return this.recipes.slice();
+  }
+}
