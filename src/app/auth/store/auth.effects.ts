ce94c7c38b8ebce2bc0b9b253471ad86c7ebc15d
Paul Zijlmans <paulzijlmans@hotmail.com>
2021-05-19 22:17:17 +0200

    NgRx - login via NgRx Effects

---
+++ src/app/auth/store/auth.effects.ts
@@ -0,0 +1,55 @@
+import { HttpClient } from '@angular/common/http';
+import { Actions, Effect, ofType } from '@ngrx/effects';
+import { of } from 'rxjs';
+import { catchError, map, switchMap } from 'rxjs/operators';
+import { environment } from '../../../environments/environment';
+import { Login, LoginStart, LOGIN_START } from './auth.actions';
+
+export interface AuthResponseData {
+  kind: string;
+  idToken: string;
+  email: string;
+  refreshToken: string;
+  expiresIn: string;
+  localId: string;
+  registered?: boolean;
+}
+
+export class AuthEffects {
+  @Effect()
+  authLogin = this.actions$.pipe(
+    ofType(LOGIN_START),
+    switchMap((authData: LoginStart) => {
+      return this.http
+        .post<AuthResponseData>(
+          'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' +
+            environment.firebaseAPIKey,
+          {
+            email: authData.payload.email,
+            password: authData.payload.password,
+            returnSecureToken: true,
+          }
+        )
+        .pipe(
+          map((resData) => {
+            const expirationDate = new Date(
+              new Date().getTime() + +resData.expiresIn * 1000
+            );
+            return of(
+              new Login({
+                email: resData.email,
+                userId: resData.localId,
+                token: resData.idToken,
+                expirationDate: expirationDate,
+              })
+            );
+          }),
+          catchError((error) => {
+            return of();
+          })
+        );
+    })
+  );
+
+  constructor(private actions$: Actions, private http: HttpClient) {}
+}
