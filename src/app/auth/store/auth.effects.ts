82e1f8fe4bfc8fe61f8bddc10b7415f05d42ba8a
Paul Zijlmans <paulzijlmans@hotmail.com>
2021-05-31 15:19:19 +0200

    replaced @Effect by createEffect

--- src/app/auth/store/auth.effects.ts
+++ src/app/auth/store/auth.effects.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
-import { Actions, Effect, ofType } from '@ngrx/effects';
+import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { of } from 'rxjs';
 import { catchError, map, switchMap, tap } from 'rxjs/operators';
 import { environment } from '../../../environments/environment';
@@ -68,128 +68,137 @@ const handleError = (errorRes: any) => {
 
 @Injectable()
 export class AuthEffects {
-  @Effect()
-  authSignup = this.actions$.pipe(
-    ofType(SIGNUP_START),
-    switchMap((signupAction: SignupStart) => {
-      return this.http
-        .post<AuthResponseData>(
-          'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' +
-            environment.firebaseAPIKey,
-          {
-            email: signupAction.payload.email,
-            password: signupAction.payload.password,
-            returnSecureToken: true,
+  authSignup = createEffect(() => {
+    return this.actions$.pipe(
+      ofType(SIGNUP_START),
+      switchMap((signupAction: SignupStart) => {
+        return this.http
+          .post<AuthResponseData>(
+            'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' +
+              environment.firebaseAPIKey,
+            {
+              email: signupAction.payload.email,
+              password: signupAction.payload.password,
+              returnSecureToken: true,
+            }
+          )
+          .pipe(
+            tap((resData) => {
+              this.authService.setLogoutTimer(+resData.expiresIn * 1000);
+            }),
+            map((resData) => {
+              return handleAuthentication(
+                +resData.expiresIn,
+                resData.expiresIn,
+                resData.localId,
+                resData.idToken
+              );
+            }),
+            catchError((errorRes) => {
+              return handleError(errorRes);
+            })
+          );
+      })
+    );
+  });
+
+  authLogin = createEffect(() => {
+    return this.actions$.pipe(
+      ofType(LOGIN_START),
+      switchMap((authData: LoginStart) => {
+        return this.http
+          .post<AuthResponseData>(
+            'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=' +
+              environment.firebaseAPIKey,
+            {
+              email: authData.payload.email,
+              password: authData.payload.password,
+              returnSecureToken: true,
+            }
+          )
+          .pipe(
+            tap((resData) => {
+              this.authService.setLogoutTimer(+resData.expiresIn * 1000);
+            }),
+            map((resData) => {
+              return handleAuthentication(
+                +resData.expiresIn,
+                resData.expiresIn,
+                resData.localId,
+                resData.idToken
+              );
+            }),
+            catchError((errorRes) => {
+              return handleError(errorRes);
+            })
+          );
+      })
+    );
+  });
+
+  authRedirect = createEffect(
+    () =>
+      this.actions$.pipe(
+        ofType(AUTHENTICATE_SUCCES),
+        tap((authSuccessAction: AuthenticateSuccess) => {
+          if (authSuccessAction.payload.redirect) {
+            this.router.navigate(['/']);
           }
-        )
-        .pipe(
-          tap((resData) => {
-            this.authService.setLogoutTimer(+resData.expiresIn * 1000);
-          }),
-          map((resData) => {
-            return handleAuthentication(
-              +resData.expiresIn,
-              resData.expiresIn,
-              resData.localId,
-              resData.idToken
-            );
-          }),
-          catchError((errorRes) => {
-            return handleError(errorRes);
-          })
+        })
+      ),
+    { dispatch: false }
+  );
+
+  autoLogin = createEffect(() => {
+    return this.actions$.pipe(
+      ofType(AUTO_LOGIN),
+      map(() => {
+        const userData: {
+          email: string;
+          id: string;
+          _token: string;
+          _tokenExpirationDate: string;
+        } = JSON.parse(localStorage.getItem('userData'));
+        if (!userData) {
+          return { type: 'DUMMY' };
+        }
+
+        const loadedUser = new User(
+          userData.email,
+          userData.id,
+          userData._token,
+          new Date(userData._tokenExpirationDate)
         );
-    })
-  );
 
-  @Effect()
-  authLogin = this.actions$.pipe(
-    ofType(LOGIN_START),
-    switchMap((authData: LoginStart) => {
-      return this.http
-        .post<AuthResponseData>(
-          'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=' +
-            environment.firebaseAPIKey,
-          {
-            email: authData.payload.email,
-            password: authData.payload.password,
-            returnSecureToken: true,
-          }
-        )
-        .pipe(
-          tap((resData) => {
-            this.authService.setLogoutTimer(+resData.expiresIn * 1000);
-          }),
-          map((resData) => {
-            return handleAuthentication(
-              +resData.expiresIn,
-              resData.expiresIn,
-              resData.localId,
-              resData.idToken
-            );
-          }),
-          catchError((errorRes) => {
-            return handleError(errorRes);
-          })
-        );
-    })
-  );
-
-  @Effect({ dispatch: false })
-  authRedirect = this.actions$.pipe(
-    ofType(AUTHENTICATE_SUCCES),
-    tap((authSuccessAction: AuthenticateSuccess) => {
-      if (authSuccessAction.payload.redirect) {
-        this.router.navigate(['/']);
-      }
-    })
-  );
-
-  @Effect()
-  autoLogin = this.actions$.pipe(
-    ofType(AUTO_LOGIN),
-    map(() => {
-      const userData: {
-        email: string;
-        id: string;
-        _token: string;
-        _tokenExpirationDate: string;
-      } = JSON.parse(localStorage.getItem('userData'));
-      if (!userData) {
+        if (loadedUser.token) {
+          const expirationDuration =
+            new Date(userData._tokenExpirationDate).getTime() -
+            new Date().getTime();
+          this.authService.setLogoutTimer(expirationDuration);
+          return new AuthenticateSuccess({
+            email: loadedUser.email,
+            userId: loadedUser.id,
+            token: loadedUser.token,
+            expirationDate: new Date(userData._tokenExpirationDate),
+            redirect: false,
+          });
+        }
         return { type: 'DUMMY' };
-      }
+      })
+    );
+  });
 
-      const loadedUser = new User(
-        userData.email,
-        userData.id,
-        userData._token,
-        new Date(userData._tokenExpirationDate)
-      );
-
-      if (loadedUser.token) {
-        const expirationDuration =
-          new Date(userData._tokenExpirationDate).getTime() -
-          new Date().getTime();
-        this.authService.setLogoutTimer(expirationDuration);
-        return new AuthenticateSuccess({
-          email: loadedUser.email,
-          userId: loadedUser.id,
-          token: loadedUser.token,
-          expirationDate: new Date(userData._tokenExpirationDate),
-          redirect: false,
-        });
-      }
-      return { type: 'DUMMY' };
-    })
-  );
-
-  @Effect({ dispatch: false })
-  authLogout = this.actions$.pipe(
-    ofType(LOGOUT),
-    tap(() => {
-      this.authService.clearLogoutTimer();
-      localStorage.removeItem('userData');
-      this.router.navigate(['/auth']);
-    })
+  authLogout = createEffect(
+    () =>
+      this.actions$.pipe(
+        ofType(LOGOUT),
+        tap(() => {
+          this.authService.clearLogoutTimer();
+          localStorage.removeItem('userData');
+          this.router.navigate(['/auth']);
+        })
+      ),
+    { dispatch: false }
   );
 
   constructor(
--- src/app/recipes/store/recipe.effects.ts
+++ src/app/recipes/store/recipe.effects.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Actions, Effect, ofType } from '@ngrx/effects';
+import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { Store } from '@ngrx/store';
 import { map, switchMap, withLatestFrom } from 'rxjs/operators';
 import { AppState } from 'src/app/store/app.reducer';
@@ -9,37 +9,41 @@ import { FETCH_RECIPES, SetRecipes, STORE_RECIPES } from './recipe.actions';
 
 @Injectable()
 export class RecipeEffects {
-  @Effect()
-  fetchRecipes = this.actions$.pipe(
-    ofType(FETCH_RECIPES),
-    switchMap(() => {
-      return this.http.get<Recipe[]>(
-        'https://recipe-book-ec58d-default-rtdb.europe-west1.firebasedatabase.app/recipes.json'
-      );
-    }),
-    map((recipes) => {
-      return recipes.map((recipe) => {
-        return {
-          ...recipe,
-          ingredients: recipe.ingredients ? recipe.ingredients : [],
-        };
-      });
-    }),
-    map((recipes) => {
-      return new SetRecipes(recipes);
-    })
-  );
+  fetchRecipes = createEffect(() => {
+    return this.actions$.pipe(
+      ofType(FETCH_RECIPES),
+      switchMap(() => {
+        return this.http.get<Recipe[]>(
+          'https://recipe-book-ec58d-default-rtdb.europe-west1.firebasedatabase.app/recipes.json'
+        );
+      }),
+      map((recipes) => {
+        return recipes.map((recipe) => {
+          return {
+            ...recipe,
+            ingredients: recipe.ingredients ? recipe.ingredients : [],
+          };
+        });
+      }),
+      map((recipes) => {
+        return new SetRecipes(recipes);
+      })
+    );
+  });
 
-  @Effect({ dispatch: false })
-  storeRecipes = this.actions$.pipe(
-    ofType(STORE_RECIPES),
-    withLatestFrom(this.store.select('recipes')),
-    switchMap(([actionData, recipesState]) => {
-      return this.http.put(
-        'https://recipe-book-ec58d-default-rtdb.europe-west1.firebasedatabase.app/recipes.json',
-        recipesState.recipes
-      );
-    })
+  storeRecipes = createEffect(
+    () =>
+      this.actions$.pipe(
+        ofType(STORE_RECIPES),
+        withLatestFrom(this.store.select('recipes')),
+        switchMap(([actionData, recipesState]) => {
+          return this.http.put(
+            'https://recipe-book-ec58d-default-rtdb.europe-west1.firebasedatabase.app/recipes.json',
+            recipesState.recipes
+          );
+        })
+      ),
+    { dispatch: false }
   );
 
   constructor(
